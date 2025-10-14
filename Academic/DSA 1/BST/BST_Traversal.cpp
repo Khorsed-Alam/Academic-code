8885b54eaf4f158e599c506338f5f75454c68444
Khorsed-Alam <khorsedalam0472@gmail.com>
2025-10-14 15:58:32 +0600

    BST traversal

---
+++ Academic/DSA 1/BST/BST_Traversal.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+Node* createNode(int value) {
+    Node* temp = new Node();
+    temp->data = value;
+    temp->left = nullptr;
+    temp->right = nullptr;
+    return temp;
+}
+
+Node* insertNode(Node* root, int key) {
+    if (root == nullptr)
+        return createNode(key);
+    if (key < root->data)
+        root->left = insertNode(root->left, key);
+    else if (key > root->data)
+        root->right = insertNode(root->right, key);
+    return root;
+}
+
+void inorder(Node* root) {
+    if (root != nullptr) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(Node* root) {
+    if (root != nullptr) {
+        cout << root->data << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
+void postorder(Node* root) {
+    if (root != nullptr) {
+        postorder(root->left);
+        postorder(root->right);
+        cout << root->data << " ";
+    }
+}
+
+int main() {
+    Node* root = nullptr;
+    root = insertNode(root, 6);
+    insertNode(root, 4);
+    insertNode(root, 7);
+    insertNode(root, 2);
+    insertNode(root, 5);
+    insertNode(root, 8);
+    insertNode(root, 3);
+
+    cout << "Inorder: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Preorder: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "Postorder: ";
+    postorder(root);
+    cout << endl;
+
+    return 0;
+}
