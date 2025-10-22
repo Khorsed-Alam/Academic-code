3ade0905ad04940d33964f1b724ce3e6aeac5f37
Khorsed-Alam <khorsedalam0472@gmail.com>
2025-10-22 18:42:43 +0600

    Q3. assingment 1

--- Academic/DSA 1/ASSIGNMENT/q3.cpp
+++ Academic/DSA 1/ASSIGNMENT/q3.cpp
@@ -7,15 +7,15 @@ using namespace std;
 bool isVal(string s){
     stack<char>stk;
     for(int i=0;i<s.length();i++){
-        char ch=s[i];
-        if(ch=='('|| ch=='{'|| ch=='['){
-            stk.push(ch);
+        char c=s[i];
+        if(c=='('|| c=='{'|| c=='['){
+            stk.push(c);
         }else{
             if(stk.empty()){
                 return false;
             }
-            char topCh=stk.top();
-            if((ch==')' && topCh!= '(') ||(ch=='}' && topCh!= '{') ||(ch==']' && topCh!= '[') ){
+            char top=stk.top();
+            if((c==')' && top!= '(') ||(c=='}' && top!= '{') ||(c==']' && top!= '[') ){
                 return false;
             }
             stk.pop();
@@ -38,3 +38,4 @@ int main(){
   
     return 0;
 }
+ 
